8964dceabe9854d1f650364e4923f73eb5b45980
Luke Dinh <lukexdinh@gmail.com>
2023-03-12 14:05:38 -0700

    Update Player.java

--- assignment_2/question_5/Player.java
+++ assignment_2/question_5/Player.java
@@ -1,9 +1,11 @@
 public class Player {
     private String name;
     private boolean expert;
+    private Team team;
 
-    public Player(String name, boolean expert) {
+    public Player(String name, boolean expert, Team team) {
         this.name = name;
         this.expert = expert;
+        this.team = team;
     }
 }
